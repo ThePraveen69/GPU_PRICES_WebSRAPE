7e4223d902c6e6d63c7f84f4288b00c2a07e4420
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-18 19:19:59 +0530

    Fetch and display HTML for user-specified GPU
    Added print statement to display the formatted HTML of the fetched page.

--- web_scrp_gpu.py
+++ web_scrp_gpu.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import re
 gpu = input("enter the product you want ?")
-url = f"https://www.newegg.com/p/pl?d={search}&N=4131"
+url = f"https://www.newegg.com/p/pl?d={gpu}&N=4131"
 page = requests.get(url).text
 doc = BeautifulSoup(page , "html.parser")
+
+
+print(doc.prettify())
